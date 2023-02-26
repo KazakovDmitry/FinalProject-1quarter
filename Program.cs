6b6816cb59bb931ba1204fc002f4834289bde879
Dmitry <dimakameya@gmail.com>
2023-02-26 19:57:19 +0500

    Печать начального массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintArray (string [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}]");
+}
+
+
+string [] startArray = {"hello", "2", "world", ":-)"};
+PrintArray(startArray);
