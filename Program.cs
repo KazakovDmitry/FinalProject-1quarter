91cba932a276396c6e4b9cf1863a95a299c71999
Dmitry <dimakameya@gmail.com>
2023-02-27 19:33:53 +0500

    Устранил ошибки

--- Program.cs
+++ Program.cs
@@ -1,29 +1,36 @@
 ﻿void PrintArray (string [] array)
 {
     Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        Console.Write($"{array[i]}, ");
+    if(array.Length == 0) Console.Write("]");
+    else
+    {    
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        Console.Write($"{array[array.Length - 1]}]");
     }
-    Console.Write($"{array[array.Length - 1]}]");
 }
 
 string [] FindElementLess3Char (string [] array)
 {
-    string temp = String.Empty;
-    for (int i = 0; i < array.Length - 1; i++)
+    string temp = String.Empty; 
+    string [] rezultArray = new string [0];
+    for (int i = 0; i < array.Length; i++)
     {
         if(array[i].Length <= 3)
             temp = temp + array[i] + " ";
     }
-    temp = temp + array[array.Length - 1];
 
-    string [] rezultArray = temp.Split(" ");
+    if(temp != String.Empty)
+    {
+        rezultArray = temp.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    }
 
     return rezultArray;
 }
 
-string [] startArray = {"hello", "2", "world", ":-)"};
+string [] startArray = {"1234", "1567", "-2456", "computer science"};
 PrintArray(startArray);
 string [] rezultArray = FindElementLess3Char(startArray);
 Console.Write(" -> ");
